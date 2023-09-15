aebfdc592ecaf4998a40824aaff9687eeea9730a
cobra-kai-03 <78267022+cobra-kai-03@users.noreply.github.com>
2023-09-15 08:08:01 +0530

    Create delete given pointer to node.cpp

---
+++ Linked List/delete given pointer to node.cpp
@@ -0,0 +1,7 @@
+void deleteNode(ListNode* node) {
+       while(node) {
+            node->val = node->next->val;
+            if(node->next != NULL && node->next->next == NULL) node->next = NULL;
+            node = node->next;
+    }
+    }
